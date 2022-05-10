3a8eb5629eba0110d92f4ef7a22b9640445b5cab
Ole Steinar L. Skrede <ole.skrede+github@pm.me>
2022-05-10 19:21:30 +0200

    remove unused stuff from copy-paste

--- js/background.js
+++ js/background.js
@@ -1,22 +1,4 @@
-
-async function createMediaSession() {
-    navigator.mediaSession.metadata = new MediaMetadata({
-        title: 'Dial up sound',
-        artist: '(Click ⏩ to open settings)'
-    })
-    navigator.mediaSession.setActionHandler('seekforward', function () {
-        chrome.runtime.openOptionsPage();
-    })
-}
-
-
 chrome.runtime.onStartup.addListener(function () {
-    // Set MediaSession API info for Chrome media player popup
-    if ('mediaSession' in navigator) {
-        createMediaSession()
-    }
-
-    // Create audio object
     var themeAudio = new Audio(chrome.extension.getURL('music/dial-up.ogg'))
     themeAudio.loop = false
     themeAudio.play()
